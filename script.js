e1250aa1ba512ad2922a6982b71a4290fbebec62
Dante Gutierrez <danteg177@gmail.com>
2024-01-10 20:49:44 -0800

    Basic Movement
    I have created functionality for detection to move the tetriminos

--- script.js
+++ script.js
@@ -6,14 +6,17 @@ let board;
 
 let running = false;
 
-let block = 
+let currentPiece = 
 {
-    tetromino: "",
+    tetrimino: "T",
     rotation: 0,
-    height: height
+    y: 0,
+    x: width / 2,
+    deltaX: 0,
+    deltaY: 0,
 };
 
-const tetrominos = 
+const tetriminos = 
 {
     "I":
     {
@@ -81,6 +84,8 @@ const tetrominos =
 
 const gameButton = document.getElementById("gameButton");
 
+const updateButton = document.getElementById("updateButton");
+
 const gameBody = document.getElementById("gameBody");
 
 const generateBoard = () => 
@@ -123,19 +128,101 @@ const changeGameState = (evt) =>
 
     if(running) 
     {
+        updateButton.disabled = false;
+
         generateBoard();
+    }
+    else
+    {
+        updateButton.disabled = true;
     }
 }
 
 const updateGame = () =>
+{
+    movePiece();
+
+    renderFrame();
+}
+
+const TogglePieceVisibility = on => 
+{
+    let currentTetrimino = tetriminos[currentPiece.tetrimino];
+
+    for(let i = 0; i < currentTetrimino.shape.length; i++)
+    {
+        for(let j = 0; j < currentTetrimino.shape[i].length; j++)
+        {
+            if(currentTetrimino.shape[i][j] !== "")
+            {
+                board[currentPiece.y + i][currentPiece.x - Math.ceil(currentTetrimino.shape[i].length / 2) + j] = on ? currentTetrimino.shape[i][j] + " " + currentTetrimino.color : "black";
+            }
+        }
+    }
+}
+
+const movePiece = () =>
+{
+    // De-render piece
+
+    let currentTetrimino = tetriminos[currentPiece.tetrimino];
+    
+    // SRS
+
+    // Always round to left
+    
+    let overlap = false;
+
+    // boundary check
+    
+    for(let i = (currentPiece.x - Math.ceil(currentTetrimino.shape[0].length)); i < (currentPiece.x + Math.floor(currentTetrimino.shape[0].length)); i++)
+    {
+        if(currentPiece.y + currentTetrimino.shape.length >= height || board[currentPiece.y + currentTetrimino.shape.length][i] !== "black")
+        {
+            overlap = true;
+
+            console.log("overlap")
+
+            break;
+        }
+    }    
+
+    if(overlap)
+    {
+        currentPiece.rotation = 0;
+        
+        currentPiece.x = width / 2;
+
+        currentPiece.y = 0;
+
+        currentTetrimino = tetriminos[currentPiece.tetrimino];
+    }
+
+    else
+    {
+        TogglePieceVisibility(false);
+
+        currentPiece.x += currentPiece.deltaX;
+
+        currentPiece.deltaX = 0;
+
+        currentPiece.y += currentPiece.deltaY;
+
+        currentPiece.deltaY = 0;
+    }
+
+    // re-render piece
+
+    TogglePieceVisibility(true);
+}
+
+const renderFrame = () =>
 {
     for(let i = 0; i < board.length; i++)
     {
         for(let j = 0; j < board[i].length; j++)
         {
             let div = document.getElementById(i + '' + j);
-
-            console.log(div.classList);
 
             if(board[i][j] !== div.className)
             {
@@ -151,4 +238,8 @@ const updateGame = () =>
 }
 
 gameButton.addEventListener("click", changeGameState);
+
+updateButton.addEventListener("click", updateGame);
+
+updateButton.disabled = true;
 
